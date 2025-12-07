5e3c9ce0b2d9a93d3f62dd8858df1c4dcc9bd247
luigi muratore <gigiomuratore@gmail.com>
2025-12-07 11:33:47 +0100

    task_0_a plots

--- src/lab04_pkg/lab04_pkg/task_0_a.py
+++ src/lab04_pkg/lab04_pkg/task_0_a.py
@@ -51,15 +51,32 @@ def main():
 
     x = [2, 4, 0]
     u = [0.8, 0.6]
-    #a = [0.001, 0.01, 0.1, 0.2, 0.05, 0.05] # noise hilghitngs for rotational
-    a = [0.12, 0, 0, 0, 0, 0] # noise hilghitngs for translational
 
-    # a[1]: noise related to translational velocity variance due to translational velocity
-    # a[2]: noise related to translational velocity variance due to rotational velocity 
-    # a[3]: noise related to rotational velocity variance due to translational velocity
-    # a[4]: noise related to rotational velocity variance due to rotational velocity
-    # a[5]: noise related to drift (pose) variance due to translational velocity
-    # a[6]: noise related to drift variance due to rotational velocity
+    #a = [0, 0, 0, 0, 0, 0] # no noise -> ideal
+
+    #a = [0, 0, 0.2, 0.2, 0, 0] # noise hilghitngs for only rotational 
+    #a = [0.2, 0.2, 0, 0, 0, 0] # noise hilghitngs for only translational 
+
+    #a = [0.01, 0.01, 0.15, 0.2, 0.02, 0.05] # noise hilghitngs for rotational and low translational
+    #a = [0.2, 0.15, 0.01, 0.01, 0.05, 0.02] # noise hilghitngs for translational and low rotational
+    
+    #a = [0.01, 0.01, 0.01, 0.01, 0.2, 0.2] # drift
+
+    #a = [0.3, 0.3, 0.3, 0.3, 0.1, 0.1] # extreme high noise
+
+   # a = [0.05, 0.15, 0.15, 0.05, 0.03, 0.03] # high coupled noise -> velocity affects rotation and vice versa
+
+    a = [0.05, 0.05, 0.05, 0.05, 0.03, 0.03] # balanced noise -> more realistic
+
+
+
+    """ noise parameters 'a'
+    a[1]: noise related to translational velocity variance due to translational velocity
+    a[2]: noise related to translational velocity variance due to rotational velocity 
+    a[3]: noise related to rotational velocity variance due to translational velocity
+    a[4]: noise related to rotational velocity variance due to rotational velocity
+    a[5]: noise related to drift (pose) variance due to translational velocity
+    a[6]: noise related to drift variance due to rotational velocity """
 
     x_prime = np.zeros([n_samples, 3]) # It gives back a n*3 matrix, 3 because we want x, y, theta
     for i in range(n_samples):
@@ -86,3 +103,49 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+"""OUTPUT TO PUT ON REPORT:
+
+[[2.38861594 4.05873356 0.3       ]
+ [2.40331724 4.06095544 0.3       ]
+ [2.43387297 4.06557349 0.3       ]
+ ...
+ [2.36011701 4.05442636 0.3       ]
+ [2.33664203 4.05087847 0.3       ]
+ [2.41955889 4.06341012 0.3       ]]
+Jacobian w.r.t state Gt:
+⎡        v⋅cos(θ)   v⋅cos(dt⋅w + θ)⎤
+⎢1  0  - ──────── + ───────────────⎥
+⎢           w              w       ⎥
+⎢                                  ⎥
+⎢        v⋅sin(θ)   v⋅sin(dt⋅w + θ)⎥
+⎢0  1  - ──────── + ───────────────⎥
+⎢           w              w       ⎥
+⎢                                  ⎥
+⎣0  0               1              ⎦
+
+Jacobian w.r.t control Vt:
+⎡  sin(θ)   sin(dt⋅w + θ)  dt⋅v⋅cos(dt⋅w + θ)   v⋅sin(θ)   v⋅sin(dt⋅w + θ)⎤
+⎢- ────── + ─────────────  ────────────────── + ──────── - ───────────────⎥
+⎢    w            w                w                2              2      ⎥
+⎢                                                  w              w       ⎥
+⎢                                                                         ⎥
+⎢ cos(θ)   cos(dt⋅w + θ)   dt⋅v⋅sin(dt⋅w + θ)   v⋅cos(θ)   v⋅cos(dt⋅w + θ)⎥
+⎢ ────── - ─────────────   ────────────────── - ──────── + ───────────────⎥
+⎢   w            w                 w                2              2      ⎥
+⎢                                                  w              w       ⎥
+⎢                                                                         ⎥
+⎣           0                                    dt                       ⎦
+
+Evaluated Gt at x=[2, 4, 0], u=[0.8, 0.6], dt=0.5:
+[[ 1.          0.         -0.05955135]
+ [ 0.          1.          0.39402694]
+ [ 0.          0.          1.        ]]
+
+Evaluated Vt at x=[2, 4, 0], u=[0.8, 0.6], dt=0.5:
+[[ 0.49253368 -0.01982058]
+ [ 0.07443918  0.09776122]
+ [ 0.          0.5       ]]
+
+"""
